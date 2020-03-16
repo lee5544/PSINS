d11b705ec211a18d16b98a899afefcf803cbb9f6
李勇 <44977031+lee5544@users.noreply.github.com>
2020-03-16 21:47:27 +0800

    工程路径初始化

--- psins200116/psinsenvi.m
+++ psins200116/psinsenvi.m
@@ -1,4 +1,4 @@
 function [rpath, dpath, mytestflag] = psinsenvi()
-	rpath = 'C:\Navigation\psins200116';
-	dpath = 'C:\Navigation\psins200116\data\';
+	rpath = 'C:\GitProject\PSINS\psins200116';
+	dpath = 'C:\GitProject\PSINS\psins200116\data\';
 	mytestflag = 0;
